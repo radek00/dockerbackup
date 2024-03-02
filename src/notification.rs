17d15203ab1d3f2b208fba8b6fc21651990f4d2d
Radekor500 <radek00500@gmail.com>
2024-03-02 16:20:33 +0100

    extend time for gotify wakeup

--- src/notification.rs
+++ src/notification.rs
@@ -12,7 +12,7 @@ pub fn send_notification(success: bool, msg: String) -> Result<(), Box<dyn std::
     }
     map.insert("title", String::from("Backup result"));
     map.insert("message", message);
-    thread::sleep(time::Duration::from_secs(30));
+    thread::sleep(time::Duration::from_secs(60));
     let client = reqwest::blocking::Client::new();
     let _req = client.post("https://gotify.radekserver.xyz/message?token=AOAga4xZ8pQ5c9Y")
             .header("Accept", "application/json")
