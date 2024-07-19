bdd8a2d1152d545b0be7198bba2bc9195e101e11
Radekor500 <radek00500@gmail.com>
2024-07-19 16:40:24 +0300

    implement functions on struct

--- src/backup/mod.rs
+++ src/backup/mod.rs
@@ -19,7 +19,7 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build() -> Result<Config, &'static str> {
+    pub fn build() -> Config {
         let date = chrono::Local::now();
         let new_dir = format!("{}-{}-{}", date.year(), date.month(), date.day());
 
@@ -62,68 +62,125 @@ impl Config {
 
         excluded_directories.push(String::from("backingFsBlockDev"));
 
-        Ok(Config {
+        Config {
             dest_path: matches.remove_one::<String>("dest_path").unwrap(),
             new_dir,
             volume_path: matches.remove_one::<PathBuf>("volume_path").unwrap(),
             excluded_directories,
             gotify_url: matches.remove_one::<String>("gotify_url"),
             discord_url: matches.remove_one::<String>("discord_url"),
-        })
+        }
     }
-}
+    pub fn backup(&self) -> Result<(), Box<dyn std::error::Error>> {
+        check_docker()?;
+        let mut err_message = String::from("");
+        let containers = check_running_containers()?;
+        let mut running_containers: Vec<&str> = containers.trim().split('\n').collect();
+        running_containers.retain(|&x| !x.is_empty());
 
-pub fn backup() -> Result<(), Box<dyn std::error::Error>> {
-    let config = Config::build()?;
-    check_docker()?;
-    let mut err_message = String::from("");
-    let containers = check_running_containers()?;
-    let mut running_containers: Vec<&str> = containers.trim().split('\n').collect();
-    running_containers.retain(|&x| !x.is_empty());
+        let err_closure = |err| {
+            err_message = format!("{}", err);
+            false
+        };
 
-    let err_closure = |err| {
-        err_message = format!("{}", err);
-        false
-    };
+        let backup_status;
 
-    let backup_status;
+        if !running_containers.is_empty() {
+            println!("Stopping containers...");
+            handle_containers(&running_containers, "stop")?;
+            backup_status = self.run().unwrap_or_else(err_closure);
+            println!("Starting containers...");
+            handle_containers(&running_containers, "start")?;
+        } else {
+            backup_status = self.run().unwrap_or_else(err_closure);
+        }
 
-    if !running_containers.is_empty() {
-        println!("Stopping containers...");
-        handle_containers(&running_containers, "stop")?;
-        backup_status = run(&config).unwrap_or_else(err_closure);
-        println!("Starting containers...");
-        handle_containers(&running_containers, "start")?;
-    } else {
-        backup_status = run(&config).unwrap_or_else(err_closure);
+        if let Some(gotify_url) = &self.gotify_url {
+            send_notification::<Gotify>(Gotify {
+                err_message: &err_message,
+                success: backup_status,
+                url: gotify_url,
+            })?;
+        }
+
+        if let Some(dc_url) = &self.discord_url {
+            send_notification::<Discord>(Discord {
+                err_message: &err_message,
+                success: backup_status,
+                url: dc_url,
+            })?;
+        }
+        Ok(())
     }
+    fn run(&self) -> Result<bool, Box<dyn std::error::Error>> {
+        let backup_status = if self.dest_path.contains('@') {
+            self.ssh_backup()
+        } else {
+            self.local_rsync_backup()
+        }?;
 
-    if let Some(gotify_url) = config.gotify_url {
-        send_notification::<Gotify>(Gotify {
-            err_message: &err_message,
-            success: backup_status,
-            url: &gotify_url,
-        })?;
+        Ok(backup_status)
     }
+    fn local_rsync_backup(&self) -> Result<bool, Box<dyn std::error::Error>> {
+        let dest_path = Path::new(&self.dest_path);
+        if !create_new_dir(dest_path, &self.new_dir)? {
+            return Err(Box::from("Could not create directory"));
+        }
 
-    if let Some(dc_url) = config.discord_url {
-        send_notification::<Discord>(Discord {
-            err_message: &err_message,
-            success: backup_status,
-            url: &dc_url,
-        })?;
+        let mut rsync = Command::new("rsync");
+
+        exclude_dirs(&mut rsync, &self.excluded_directories);
+
+        let exec_rsync = rsync
+            .arg("-az")
+            .arg(&self.volume_path)
+            .arg(dest_path.join(&self.new_dir))
+            .status()?;
+        if exec_rsync.success() {
+            return Ok(true);
+        }
+        Err(Box::from("Rsync backup failed"))
     }
-    Ok(())
-}
+    fn ssh_backup(&self) -> Result<bool, Box<dyn std::error::Error>> {
+        let mut tar_volumes = Command::new("tar");
 
-fn run(config: &Config) -> Result<bool, Box<dyn std::error::Error>> {
-    let backup_status = if config.dest_path.contains('@') {
-        ssh_backup(config)
-    } else {
-        local_rsync_backup(config)
-    }?;
+        tar_volumes.arg("-cf-").arg("-C").arg(&self.volume_path);
 
-    Ok(backup_status)
+        exclude_dirs(&mut tar_volumes, &self.excluded_directories);
+
+        let tar_exec = tar_volumes.arg(".").stdout(Stdio::piped()).spawn()?;
+
+        let ssh_path_parts: Vec<&str> = self
+            .dest_path
+            .splitn(2, std::path::MAIN_SEPARATOR)
+            .collect();
+
+        if ssh_path_parts.len() != 2 {
+            return Err(Box::from("Wrong path format"));
+        }
+
+        let dest_path = Path::new(ssh_path_parts[1]).join(&self.new_dir);
+
+        let ssh = Command::new("ssh")
+            .arg(ssh_path_parts[0])
+            .arg("mkdir")
+            .arg(&dest_path)
+            .arg("&&")
+            .arg("tar")
+            .arg("-C")
+            .arg(dest_path)
+            .arg("-xf-")
+            .stdin(Stdio::from(tar_exec.stdout.unwrap()))
+            .output()?;
+
+        if ssh.status.success() {
+            return Ok(true);
+        }
+        Err(Box::from(format!(
+            "Ssh backup failed: {}",
+            String::from_utf8_lossy(&ssh.stderr)
+        )))
+    }
 }
 
 fn check_docker() -> Result<(), Box<dyn std::error::Error>> {
@@ -140,68 +197,6 @@ fn check_running_containers() -> Result<String, Box<dyn std::error::Error>> {
         .output()?;
     let containers_list = String::from_utf8(running_containers.stdout)?;
     Ok(containers_list)
-}
-
-fn local_rsync_backup(config: &Config) -> Result<bool, Box<dyn std::error::Error>> {
-    let dest_path = Path::new(&config.dest_path);
-    if !create_new_dir(dest_path, &config.new_dir)? {
-        return Err(Box::from("Could not create directory"));
-    }
-
-    let mut rsync = Command::new("rsync");
-
-    exclude_dirs(&mut rsync, &config.excluded_directories);
-
-    let exec_rsync = rsync
-        .arg("-az")
-        .arg(&config.volume_path)
-        .arg(dest_path.join(&config.new_dir))
-        .status()?;
-    if exec_rsync.success() {
-        return Ok(true);
-    }
-    Err(Box::from("Rsync backup failed"))
-}
-
-fn ssh_backup(config: &Config) -> Result<bool, Box<dyn std::error::Error>> {
-    let mut tar_volumes = Command::new("tar");
-
-    tar_volumes.arg("-cf-").arg("-C").arg(&config.volume_path);
-
-    exclude_dirs(&mut tar_volumes, &config.excluded_directories);
-
-    let tar_exec = tar_volumes.arg(".").stdout(Stdio::piped()).spawn()?;
-
-    let ssh_path_parts: Vec<&str> = config
-        .dest_path
-        .splitn(2, std::path::MAIN_SEPARATOR)
-        .collect();
-
-    if ssh_path_parts.len() != 2 {
-        return Err(Box::from("Wrong path format"));
-    }
-
-    let dest_path = Path::new(ssh_path_parts[1]).join(&config.new_dir);
-
-    let ssh = Command::new("ssh")
-        .arg(ssh_path_parts[0])
-        .arg("mkdir")
-        .arg(&dest_path)
-        .arg("&&")
-        .arg("tar")
-        .arg("-C")
-        .arg(dest_path)
-        .arg("-xf-")
-        .stdin(Stdio::from(tar_exec.stdout.unwrap()))
-        .output()?;
-
-    if ssh.status.success() {
-        return Ok(true);
-    }
-    Err(Box::from(format!(
-        "Ssh backup failed: {}",
-        String::from_utf8_lossy(&ssh.stderr)
-    )))
 }
 
 fn exclude_dirs(command: &mut Command, dirs_to_exclude: &Vec<String>) {
