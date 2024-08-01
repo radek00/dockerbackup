887440f6335b9721d3a86576a4e4164c622bc8e0
Radekor500 <radek00500@gmail.com>
2024-08-01 21:49:52 +0300

    accept multiple destination paths

--- src/backup/utils.rs
+++ src/backup/utils.rs
@@ -43,26 +43,39 @@ pub fn handle_containers(containers: &Vec<&str>, command: &str) -> Result<(), Ba
     Err(BackupError::new("Error handling containers"))
 }
 
-pub fn validate_destination_path(val: &str) -> Result<(String, TargetOs), String> {
-    if val.contains('@') {
-        let tuple: Vec<&str> = val.splitn(2, ',').collect();
-        if tuple.len() != 2 {
-            return Err(String::from(
-                "Destination path and target os must be provided",
-            ));
-        }
+pub fn validate_destination_path(val: &str) -> Result<Vec<(String, TargetOs)>, String> {
+    let mut paths: Vec<(String, TargetOs)> = Vec::new();
+    let collect_path = |path: &str| {
+        if path.contains('@') {
+            let tuple: Vec<&str> = path.splitn(2, ',').collect();
+            if tuple.len() != 2 {
+                return Err(String::from(
+                    "Destination path and target os must be provided",
+                ));
+            }
+    
+            let parts: Vec<&str> = tuple[0].splitn(2, ':').collect();
+            if parts.len() == 2 && parts[0].contains('@') {
+                Ok((tuple[0].to_owned(), TargetOs::from_str(tuple[1])?))
+            } else {
+                Err(String::from(
+                    "SSH path must be in the format user@host:path",
+                ))
+            }
+        } else if Path::new(path).exists() {
+            Ok((path.to_owned(), TargetOs::Windows))
 
-        let parts: Vec<&str> = tuple[0].splitn(2, ':').collect();
-        if parts.len() == 2 && parts[0].contains('@') {
-            Ok((tuple[0].to_owned(), TargetOs::from_str(tuple[1])?))
         } else {
-            Err(String::from(
-                "SSH path must be in the format user@host:path",
-            ))
+            Err(String::from("Local path does not exist"))
         }
-    } else if Path::new(val).exists() {
-        Ok((val.to_owned(), TargetOs::Windows))
+    };
+    if val.contains('^') {
+        let destinations: Vec<&str> = val.split('^').filter(|el| !el.is_empty()).collect();
+        for destination in destinations {
+            paths.push(collect_path(destination)?);
+        }
     } else {
-        Err(String::from("Local path does not exist"))
+        paths.push(collect_path(val)?);
     }
+    Ok(paths)
 }
