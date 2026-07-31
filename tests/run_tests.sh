a276d87f53d6d54272c799a198bae1cb4f1764c0
radek00 <radekw00@proton.me>
2026-07-31 15:44:37 +0200

    run integration tests on build validation action

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+cleanup() {
+    docker compose down -v --remove-orphans
+}
+trap cleanup EXIT
+
 docker compose up -d --build
 
 docker compose exec test-runner bash ./tests/test_script.sh
@@ -8,5 +13,3 @@ docker compose exec test-runner bash ./tests/test_edge_cases.sh
 docker compose exec test-runner bash ./tests/test_container_lifecycle.sh
 docker compose exec test-runner bash ./tests/test_multi_destination.sh
 docker compose exec test-runner bash ./tests/test_interrupt.sh
-
-docker compose down -v --remove-orphans
